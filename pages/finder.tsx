f74d895e820b6e1ccab337b2f1befcc5315af68d
alorslouis <alorslouis@users.noreply.github.com>
2022-12-28 20:22:41 -0800

    Create finder.tsx

---
+++ pages/finder.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import saddles from "../assets/data/saddles.json";
+
+export default function Finder() {
+  const f = saddles;
+
+  const brands = new Set(f.map((f) => f.Brand));
+  console.log(brands);
+  console.log(f);
+
+  const [activeSaddle, setActiveSaddle] = useState<typeof f[0] | null>(null);
+  console.log(activeSaddle);
+
+  const active = f ? "active" : "";
+
+  const FF = (saddle: typeof f[0]) => {
+    const ff = [];
+    for (let i = 0; i < Object.keys(f).length; i++) {
+      const t = Object.values(saddle)[i];
+      if (t.length > 3 && t.startsWith("YES")) {
+        ff.push(t);
+      } else if (t.length === 3 && t.startsWith("YES")) {
+        ff.push(t);
+      }
+    }
+    return (
+      <div>
+        <h3>{saddle.Brand}</h3>
+        <h3>{saddle.Model}</h3>
+      </div>
+    );
+  };
+
+  const aa = activeSaddle ? "active" : "";
+
+  return (
+    <div className="container mx-auto text-center">
+      <h1 className="text-4xl font-bold uppercase my-8">
+        saddle compatibility
+      </h1>
+      <h2 className="text-xl">find the right fit</h2>
+      <label htmlFor="1">brand</label>
+      <select placeholder="brand">
+        <option id="1" value="1">
+          1
+        </option>
+      </select>
+      <div className="flex">
+        <div className="flex flex-wrap gap-8 justify-center">
+          <ul className="max-h-full overflow-scroll">
+            {f.map((f) => {
+              return (
+                <li
+                  key={f.Model}
+                  onClick={() => setActiveSaddle(f)}
+                  className="my-4 flex flex-col rounded-lg border-rounded border-2 border-white p-4 hover:border-red-800 transition-all ease-in-out"
+                >
+                  <h3 className="font-bold text-lg">{f.Brand}</h3>
+                  <h3>{f.Model}</h3>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div>
+          {activeSaddle !== null && <div>ttttt{activeSaddle.Brand}</div>}
+        </div>
+      </div>
+    </div>
+  );
+}
